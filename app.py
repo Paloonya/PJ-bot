e19e8ddc5e64367b51b3084a27d946a028d0f618
Paloonya <pavlik0101@gmail.com>
2023-04-24 22:29:40 +0300

    Add existing file

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, redirect, url_for, request, render_template, session
+import psycopg2
+
+def get_db_connection():
+    con = psycopg2.connect(database="Tests_DB",
+                           user="Paloonya",
+                           password="01012003",
+                           host="127.0.0.1",
+                           port="5432"
+                           )
+    cur = con.cursor()
+    cur.execute("SELECT ques from quest_raw")
+    rows = cur.fetchall()
+    for row in rows:
+        print("Question: ", row[0])
+    con.close()
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def index():
+    con = get_db_connection()
+    return render_template('index.html')
